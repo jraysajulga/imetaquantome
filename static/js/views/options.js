8370f7bd971175564f93110b18a346c5ec8abd56
jraysajulga <jraysajulga@gmail.com>
2019-03-25 16:17:27 -0500

    configure to close dropdowns when appropriate

--- static/js/views/options.js
+++ static/js/views/options.js
@@ -17,6 +17,22 @@ define([''],
                                         text : text})
             var dropdown_content = $("<div>", {class : "option-dropdown-content"})
 
+            // CLose dropdown menus if user clicks outside
+            window.onclick = function(event) {
+                if (!event.target.matches('.option-dropbtn')) {
+                    var dropdowns = document.getElementsByClassName("option-dropdown-content");
+                    for (var i = 0; i < dropdowns.length; i++){
+                        var shownDropdown = dropdowns[i];
+                        if ($( shownDropdown ).css("display") == "block"){
+                            $( shownDropdown ).toggle("show");
+                        }
+                        //if (shownDropdown.classList.contains("show")) {
+                          //  shownDropdown.classList.remove("show");
+                        //}
+                    }
+                }
+            }
+
             // Iterate through dataset's headers and place within dropdown.
             var columns = this.model.get("headers");
             for (var i = 0; i < columns.length; i++){
@@ -26,7 +42,11 @@ define([''],
                                             console.log($( this ).html());
                                         }}));
             }
-            dropdown.append($("<button>", {class : "option-dropbtn"}));
+            dropdown.append($("<button>", {class : "option-dropbtn",
+                                       click : function(){
+                                        //$( this ).find(".option-dropdown-content").classList.toggle("show")
+                                        $(this).parent().find(".option-dropdown-content").toggle("show");
+                                       }}));
             dropdown.append(dropdown_content);
             return dropdown
         },
