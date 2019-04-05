0aee7292209d8dcfd6db8705a3bbe42fb2e4c316
jraysajulga <jraysajulga@gmail.com>
2019-04-04 20:00:46 -0500

    implement lighter color function

--- static/js/views/table.js
+++ static/js/views/table.js
@@ -28,17 +28,23 @@ define([''],
             var highlight_plot;
             var index;
             var colors = this.model.get("colors"); 
-            console.log(highlights);
-            console.log(colors);
             $(this.table.cells().nodes()).css("background-color", "white");
             for (var i = 0; i < highlights.length; i++){
                 highlight_plot = highlights[i];
                 for (key in highlight_plot) {
                     index = this.headers.indexOf(highlight_plot[key])
-                    console.log(key)
-                    $(this.table.column(index).nodes()).css("background-color", colors[key][0].split(";")[0]);
+                    $(this.table.column(index).nodes()).css("background-color", this.adjustColor(colors[key][0].split(";")[0], 30));
                 }
             }
+        },
+
+        adjustColor : function(color, percent) {
+            var num = parseInt(color.replace("#", ""),16),
+                amt = Math.round(2.55 * percent),
+                R = (num >> 16) + amt,
+                B = (num >> 8 & 0x00FF) + amt,
+                G = (num & 0x0000FF) + amt;
+                return "#" + (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (B<255?B<1?0:B:255)*0x100 + (G<255?G<1?0:G:255)).toString(16).slice(1);
         },
 
         render : function() {
